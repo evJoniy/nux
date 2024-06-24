27903759e565acc7badf60f72cef48d847952b77
Yevhenii Turianskyi <zhenya5990@gmail.com>
2024-06-24 21:40:44 +0300

    Utilize AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,20 @@
 
 namespace App\Providers;
 
+use App\Repositories\HistoryRepository;
+use App\Repositories\Interfaces\HistoryRepositoryInterface;
+use App\Repositories\Interfaces\LinkRepositoryInterface;
+use App\Repositories\Interfaces\UserRepositoryInterface;
+use App\Repositories\LinkRepository;
+use App\Repositories\UserRepository;
+use App\Services\HistoryService;
+use App\Services\Interfaces\HistoryServiceInterface;
+use App\Services\Interfaces\LinkServiceInterface;
+use App\Services\Interfaces\LuckyServiceInterface;
+use App\Services\Interfaces\UserServiceInterface;
+use App\Services\LinkService;
+use App\Services\LuckyService;
+use App\Services\UserService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +25,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->singleton(HistoryServiceInterface::class, HistoryService::class);
+        $this->app->singleton(LinkServiceInterface::class, LinkService::class);
+        $this->app->singleton(LuckyServiceInterface::class, LuckyService::class);
+        $this->app->singleton(UserServiceInterface::class, UserService::class);
+
+        $this->app->singleton(HistoryRepositoryInterface::class, HistoryRepository::class);
+        $this->app->singleton(LinkRepositoryInterface::class, LinkRepository::class);
+        $this->app->singleton(UserRepositoryInterface::class, UserRepository::class);
+        //or explode to "domain" providers
     }
 
     /**
