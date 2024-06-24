8e26b76a105364c28f99ddb1dd7635a1922d5a18
Yevhenii Turianskyi <zhenya5990@gmail.com>
2024-06-24 17:40:24 +0300

    Create blade for register form

---
+++ resources/views/register.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration Form</title>
+    <style>
+        .container {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+        }
+        form {
+            display: flex;
+            flex-direction: column;
+            gap: 10px;
+        }
+        h1 {
+            margin-bottom: 20px;
+        }
+    </style>
+</head>
+<body>
+<div class="container">
+    <h1>Registration Form</h1>
+    <div>
+        <form action="{{ route('register.store') }}" method="POST">
+            @csrf
+            <div>
+                <label for="username">Username:</label>
+                <input type="text" id="username" name="username" required>
+            </div>
+            <div>
+                <label for="mobile">Phone number:</label>
+                <input type="text" id="mobile" name="mobile" required>
+            </div>
+            <div>
+                <button type="submit">Register</button>
+            </div>
+        </form>
+    </div>
+</div>
+</body>
+</html>
